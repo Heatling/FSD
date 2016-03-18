0abab321950fa9686467112db0f27674ff98c6d7
s123791 <s123791@student.dtu.dk>
2016-03-18 12:44:48 +0100

    Removed old main fucntions

--- Program.cs
+++ Program.cs
@@ -8,78 +8,11 @@ namespace RINGSDrawing
 {
 	class Program
 	{
-		static void r1Main(string[] args)
+		static void Main(string[] args)
 		{
-			Node[] children = new Node[9];
-			for(int i = 0; i<children.Length; i++)
-			{
-				if(i <6)
-				{
-					children[i] = new StaticNode(
-									new Node[] { new StaticNode(new Node[] { })
-												, new StaticNode(new Node[] {
-													new StaticNode(new Node[] { })
-													, new StaticNode(new Node[] { })
-												})
-												, new StaticNode(new Node[] { })
-												, new StaticNode(new Node[] { })
-									});
-				}
-				else
-				{
-
-					children[i] = new StaticNode(new Node[] { });
-				}
-				
-			}
-
-			Node root = new StaticNode(children);
-			CircleNode layout = RINGS.MakeLayout(root, 350);
-			Console.WriteLine("Circles:");
-			Circle tempC;
-			
-			Console.ReadKey();
-			RINGSForm f = (new RINGSForm(700,700));
-			f.Show();
-			f.DrawAllCircles(layout);
-			Console.ReadKey();
-		}
-
-		static void t2Main(string[] args)
-		{
-			string t = "<drive name = \"C:\">\n"+
-				"<directory name = \"d1\">Unauthorized</directory>" +
-				"<directory name = \"d2\"><file name = \"file2\"></file><directory name = \"file3\"></directory></directory>" +
-					"</drive>";
-
-			Tag r = new Tag(null);
-
-			HomeMadeXMLReader.getTagFromSubstring(t, 0, r);
-
-			foreach(Tag child in r.GetChildren())
-			{
-				printTagAndChildren(child, 0);
-			}
-			Console.ReadKey();
+			t4Main(args);
 		}
 		
-		static void t3Main(String[] args)
-		{
-			//Real world test
-			Tag cDrive = HomeMadeXMLReader.extractFileSystem(
-				@"C:\Users\Emad\Dropbox\DTU\Bachelor projekt\File system screenshots\fileSystem ss 26-02-16.xml", "");
-			
-			CircleNode layout = RINGS.MakeLayout(cDrive, 350);
-			Console.WriteLine("Circles:");
-			Circle tempC;
-
-			Console.ReadKey();
-			RINGSForm f = (new RINGSForm(700,700));
-			f.Show();
-			f.DrawAllCircles(layout);
-			Console.ReadKey();
-		}
-
 		static void t4Main(string[] args)
 		{
 			string t = "<drive name = \"C:\">\n" +
@@ -103,13 +36,13 @@ namespace RINGSDrawing
 			f.Show();
 			f.DrawAllCircles(layout);
 			Console.ReadKey();
-			Console.WriteLine("Saving image");
-			f.Refresh();
-			f.drawToFile(@"C:\Users\Emad\Desktop\fileSystem.png");
+			//Console.WriteLine("Saving image");
+			//f.Refresh();
+			//f.drawToFile(@"C:\Users\Emad\Desktop\fileSystem.png");
 			Console.ReadKey();
 		}
 
-		static void Main(string[] args)
+		static void t5Main(string[] args)
 		{
 
 			Tag r = XMLReaderToTree.extractFileSystem(
